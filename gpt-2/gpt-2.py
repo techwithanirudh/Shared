bef71a367eb2e0727ecc87074fca6719f6d06e74
techwithanirudh <70373803+techwithanirudh@users.noreply.github.com>
2020-11-03 19:47:18 +0530

    Update gpt-2.py

--- gpt-2/gpt-2.py
+++ gpt-2/gpt-2.py
@@ -3,10 +3,11 @@ from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
 import time
 
+path = 'C:/Users/Anirudh/AppData/Local/Programs/Python/Python38/Lib/site-packages/myModules/browser/chromedriver.exe'
 query = input('Text: ')
 chromeOptions = Options()
 chromeOptions.headless = True
-browser = webdriver.Chrome(executable_path='C:\\Users\Anirudh\AppData\Local\Programs\Python\Python38\Lib\site-packages\myModules\\browser\chromedriver.exe', options=chromeOptions)
+browser = webdriver.Chrome(executable_path=path, options=chromeOptions)
 browser.get('https://huggingface.co/gpt2?text={}'.format(query))
 time.sleep(5)
 elem = browser.find_element_by_class_name('output-panel')
