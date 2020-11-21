7f0d64da41296e583c424096b0cca91ceae0eda7
techwithanirudh <70373803+techwithanirudh@users.noreply.github.com>
2020-11-21 18:17:58 +0530

    commit

---
+++ Image Classifier/resize.py
@@ -0,0 +1,19 @@
+import os
+from PIL import Image
+
+os.chdir(os.path.dirname(__file__))
+
+fileFormat = '.png'
+w, h = 32, 32
+imgNames = os.listdir('.')
+imgs = []
+for imgName in imgNames:
+    if imgName.endswith(fileFormat):
+        imgs.append(imgName)
+ind = 0
+for img in imgs:
+    img = os.path.dirname(__file__) + '/' + img
+    print(img)
+    image = Image.open(img)
+    image = image.resize((w, h), Image.ANTIALIAS)
+    image.save(f'{img}')
