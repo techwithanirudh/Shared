deb9a9b25c11f139ee1812fedbe9bf32681b63a1
techwithanirudh <70373803+techwithanirudh@users.noreply.github.com>
2020-11-21 19:06:25 +0530

    commit

--- Image Classifier/test.py
+++ Image Classifier/test.py
@@ -1,11 +1,19 @@
+from logging import root
 import cv2 as cv
 import numpy as np
 import matplotlib.pyplot as plt
 from tensorflow.keras import datasets, models
 import os
+from tkinter import filedialog
+import tkinter
 
 os.chdir(os.path.dirname(__file__))
 
+openWin = filedialog.Tk()
+openWin.withdraw()
+imgname = filedialog.askopenfilename(filetypes=[('PNG', '*.png')])
+if not imgname.endswith('.png'):
+    imgname = imgname + '.png'
 (training_images, training_labels), (testing_images, testing_labels) = datasets.cifar10.load_data()
 training_images, testing_images = training_images / 255, testing_images / 255
 class_names = ['Plane', 'Car', 'Bird', 'Cat', 'Deer', 'Dog', 'Frog', 'Horse', 'Ship', 'Truck']
@@ -14,10 +22,12 @@ training_labels = training_labels[:20000]
 testing_images = testing_images[:4000]
 testing_labels = testing_labels[:4000]
 model = models.load_model('image_classifier.model')
-img = cv.imread('horse.png')
-img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
-plt.imshow(img, cmap=plt.cm.binary)
-predection = model.predict(np.array([img]) / 255)
-index = np.argmax(predection)
-print(f'Prediction Is: {class_names[index]}')
-plt.show()
+if imgname != '':
+    img = cv.imread(imgname)
+    img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+    plt.imshow(img, cmap=plt.cm.binary)
+    predection = model.predict(np.array([img]) / 255)
+    index = np.argmax(predection)
+    plt.xlabel(class_names[index], fontdict={'Courier New': 24})
+    plt.show()
+    print(f'Prediction Is: {class_names[index]}')
