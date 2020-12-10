ce570fd8dc97d4db1a690b92e352a5f0eba03deb
techwithanirudh <70373803+techwithanirudh@users.noreply.github.com>
2020-12-10 22:20:24 +0530

    Create findxycoturesofimage.py

---
+++ findxycoturesofimage.py
@@ -0,0 +1,29 @@
+import cv2 
+
+# Let's load a simple image with 3 black squares 
+image = cv2.imread('D:/Anirudh/pythonprojects/sampleproject1/data/Images/5/scratch1.png') 
+# Grayscale 
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) 
+
+# Find Canny edges 
+edged = cv2.Canny(gray, 30, 200) 
+
+# Finding Contours 
+# Use a copy of the image e.g. edged.copy() 
+# since findContours alters the image 
+contours, hierarchy = cv2.findContours(edged, 
+	cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE) 
+
+cv2.imshow('Canny Edges After Contouring', edged) 
+print("Number of Contours found = " + str(len(contours))) 
+
+# Draw all contours 
+# -1 signifies drawing all contours 
+cv2.drawContours(image, contours, -1, (0, 255, 0), 3) 
+for contour in contours:
+    x, y, _, _ = cv2.boundingRect(contour)
+    kx = contours[contour][:0,0]
+    ky = contours[contour][:0,1]
+cv2.imshow('Contours', image) 
+cv2.waitKey(0) 
+
