bb5ae84cf3adb4a6da1301f3dfc6d5bfc42c187a
techwithanirudh <70373803+techwithanirudh@users.noreply.github.com>
2020-12-18 19:31:32 +0530

    Update controlMouseInPython.py

--- controlMouseInPython.py
+++ controlMouseInPython.py
@@ -14,11 +14,11 @@ while True:
     contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     for c in contours:
         area = cv2.contourArea(c)
-        if area > 300:
+        if area > 300: 
             x, y, w, h = cv2.boundingRect(c)
             cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-            if y < prev_y:
-                pyautogui.press('space')
+            if prev_y - y > 10:
+                pyautogui.press('down')
                 # print('moving down')
             prev_y = y
             # print(area)
