4a76e9c3d026a647c94249c97ed5ee0b8ba9d061
techwithanirudh <70373803+techwithanirudh@users.noreply.github.com>
2020-12-19 16:40:30 +0530

    Create testWHATTHENOTING.py

---
+++ testWHATTHENOTING.py
@@ -0,0 +1,9 @@
+import time
+from myModules import browser
+import pyqrcode 
+
+browser = browser.Chrome()
+browser.get('https://web.whatsapp.com/')
+time.sleep(3)
+element = browser.find_element_by_class_name('_3l6Cf')
+code = element.get_attribute('innerHTML')
