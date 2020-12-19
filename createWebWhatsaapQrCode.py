c86fe146a39f39e34e344832d814c66b556d1456
techwithanirudh <70373803+techwithanirudh@users.noreply.github.com>
2020-12-19 13:44:13 +0530

    Update createWebWhatsaapQrCode.py

--- createWebWhatsaapQrCode.py
+++ createWebWhatsaapQrCode.py
@@ -5,7 +5,7 @@ os.chdir(os.path.dirname(__file__))
 
 code = open('codeOfWhatsApp.txt').read()
 code = code.split('data-ref="')[1]
-code = code.split('" style=""')[0]
+code = code.split('"><span>')[0]
 print(code)
 code = pyqrcode.create(code) 
 code.show() 
