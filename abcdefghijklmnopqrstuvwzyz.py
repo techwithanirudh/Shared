9406d52c6dbc484fb8c9300f1e51f487e084e05a
techwithanirudh <70373803+techwithanirudh@users.noreply.github.com>
2020-12-21 14:13:17 +0530

    Update abcdefghijklmnopqrstuvwzyz.py

--- abcdefghijklmnopqrstuvwzyz.py
+++ abcdefghijklmnopqrstuvwzyz.py
@@ -1,1 +1,182 @@
-.encode('')
+Name: Python
+Message: w53DnMOgw6LDl8OXwpLDmcOdw5DDlG7Di8OCw6jDocKIw5zDp8OBw57DlMOow5HDiMOlwoXDncOQw5_Dk8OXw4TCmcOWw5rDnsOlw6fDlMOifcOOw5DDosOUw6bDl8KPw5TDnsOBw6vDl8OXw5PDk8KUecOWw6XDlMOQwpLDmMOZw4_DlMOSw47DhcOmwqLDn8OUw5DDmMOhw5nDqcOKw5XCoMOYw6nDk8Ofw5PDl8OEwpnDncOVw5_DncOmw6PCkMOYw53Dk8OXw4jDqMOIw5PDg8OIwr_Dp8OYw5HDo8OXw6PDncOjwpPDhsOWwpLDisOXbcOVw5bDlMK9wpnDp8ONw5vDk8Oiw5jDpcOgwpPDmsOXw4fDmMOVw5jDmsOKw4LCp8Onw53Dn8Oew6PDocOkwqHDmsOMwpLDjsOhw5PDnsOWw5lww5DDmcOKwrPDoMOdw6XDlcOlwrzDhMObw5l-w4nDocOTw5Jww6zDmcOUw5TDnMOdw6TDncKhw5zDiMOUw4nDpsOMw6XDicOXfsOcw6PDlcOcw53DosKdw5vDmMOew5bCksOOw6HDk8Oew5bDmXDDhMOZw6HDonjDmsOhw5_DoMKFw5bDl8ORw5nDkcOYw5nDkn7DsMOZw4rDk8Ogw53DpcOVw6XCk8OGw6HDksOhw5LDncKSw4fDicKZw53DlcOfw53DpsOjwpDCtcOebcOYw5fDo8OQwo_Dl8OKwrzDnsOiw5HDpMObwqLDpsOVw5XDicOVw5vDm8OZw5XCncOKw47DgsOew5rDl8OnwpzDo8Ofw6TDnMOUw5HDpcKFw53DkMOfw5PDl8OEwpnDg8OYw6PDl8Ojw53Do31vw5LDosOZw53DksOdw5fChcKNwpnDg8OYw6PDl8Ojw53Do8Kbwo5tw6HDlcOow4zDnsOSw5h-w5rDmMOMw47Dj8Omw5bDpcOgw4rDkcOmwo3ClsKQwpzDjMOKwrHDncOgw43DosOhwpbCmMKQwpNvw4XDpMOUw6vDlsOUw5bChcKNwpnDlsOaw57DpcOnw5TDosKhwqvDjMOkw4rDmsOSw6fCjMOUw4DDrcOdw5fDncOhwrHDnsOgw6fDjsOSw6DDmMKdbcORw5bDlMOHw6zDmcOawp3DlcOZw6PCmMKaw43Dl8Omw5XDp8Kdwp7Ck8OcwrXDm8Kiw5_Dl8OPw6jDosORw6PDlcKRw5XDlMOhwpLClsKNb8OEw6LDocONwp3DocOgw5TDlcOjwo3ClcKbb8OZw4_DlMORw4rCvsOtwpTCpcKPw5DDpsOew6fDpsOKw5XCoMOLw53DkcOTw4PDisK8w57DocONw53DosOTw5HDqcOSw4jDj8OTw5jDp8OCw53DhcOSwrXCocKbw4fCosOawqrCssOWwprCjm3DlcOUw5jDiMKPwqHChcK1w6XDmcOVw5TDnMOowp3Dl8OYw5nDgsOTw5nDqMOVw5jDhsOaw4TDnsKcwo_DmMOcw6LDlMOiwrvCucKwwr7CjMKdbcOSw5PDicK1wpnCscKIw5LDncOYw5TCnsOmw5XDj8Obw5nCnMKKw5PDhcOZwrHCpsOmw43DlcKrwpbClsKZw47ClsOAfMOIw6PDh8OUwoTConDDnMOjw4zDlMKcw6fDn8Ocw5zDmcKLwpnCh8Kywp_DosOUw4bCvsK3wpvCkcOKwp7DkXnDoMOlw47DkcOmwo3Dl8OSw5PDicKOWsOcw6PDjMOUwo7CscKPw6DDrMOWw5XDlcOUw5jDiMKdw4fDl8K1w5rDqMONwpfDkcOjw5PDlcKcwoVtw5XDlMOYw4jCncOXw43Cv8OwwpzCkcKPeMOow5jDncOYwpPDlsOew4rDmcOTwpfClcKVecKDw6vDicOYw6LClMKswpDDisOKw4XCtsOXw53DmcOUw5bCvMKxw6LDqMKQw5HDoMOjw6bDo8OYw5fCj8KSwpvCpMKTwphuw5nCscOrw5vDjcOjwo7CscKPwpfDhsONw4TDnsOOw6LDjMKWbsOYw4TDq8Odw5bDlsKOwrHCj8KSwrvDmsOVw6TDhsOtwoPDo8OMw47Dg8KZw6vDl8Ohw5nDmcOTwpHCk8K1w5zDpsONw6PDkcKPw5fDisK-w63ClMOJwo_Dm8OZw6LDo8OUw4zDiMKTwod-w5vDjsOFw5fCt8KZwrHCiMKWwo7Co8Kew6PDo8OGw5HDjcOIw6PDkcOjw4XDjsK-w6zCnMKow6PDl8Oow5vDlcKfwoXCisKSwpDClMKKw4vChsKMcMKkwpTDnMOQw6DDm8OUw6TCk8KQwoPCmcOBwpbCisKPwo_ChXfCosORwo95w6LDlcOhw5fDmMOZwoPCr8KFw6vDhMOYw5jCk8OFw6fDqMORw5vClsOZw5LCnsOjw5fDiMOlw4rDosOGw5TDg8OUwrbDmMOZw5TDlMObw5nDncOkw5LDkcOSw5XDhsOow4jDk8KMwo3CksOywqLDgMK_wq_DiMK3wpzCk8Odw4LDk8OXw5vCjMKYwo1vw4TDmsOmw4_DlMOiwqLDksOcw5zDiMOOwprCjn7DjMOdw5TDmsOEw5jDlsOXw6fCjsKxwo_DksOlw5TDmsOlw4rDpsKRw5XDjcOTwrTDmMOZw5TDlMObw5nDncOkw5LDh8Ocw5HDncOkw4TDo8OMwo13wqjCo8KSw4rCrsOdw5PCrcKVw5LDhMObw5PClsOAwp7DisOUwr_DrcOZw5rCnsOSw53DpcOLwqTDgsKSw5bDjsOqwr7CocOBwpTCtMOiw6rCl8OTw5fDqsOKwqLDkMKMwox8w47DosOTw6TDmMOEwrLDqMOswpbDosOTw6LDk8OPw57DisOcw6XCjcOnw5fDocONw5PCt8KifsORw53DnsOpw6PDj8OVw5TDm8Kgw5jDmcORw5PDg8OQwrXDssOnwpDCusOTw63DosKewrjCs8K3wrfCt8Kd
+Key: xxxxxxxxxxxxx
+
+# import tkinter module 
+from tkinter import *
+# import datetime module
+from datetime import datetime
+# import Encryption and Decryption module
+import base64 as encoder_and_decoder
+
+logging_file_path = "D:\Anirudh\pythonprojects\sampleproject1\l.txt"
+
+# creating root object 
+root = Tk() 
+
+# defining size of window 
+root.geometry("1200x6000") 
+  
+# setting up the title of window 
+root.title("Secret Message Encryption and Decryption") 
+  
+Tops = Frame(root, width = 1600, relief = SUNKEN) 
+Tops.pack(side = TOP) 
+  
+f1 = Frame(root, width = 800, height = 700, 
+                            relief = SUNKEN) 
+f1.pack(side = LEFT) 
+
+lblInfo = Label(Tops, font = ('helvetica', 50, 'bold'), 
+          text = "Secret Message \n Encryption and Decryption", 
+                     fg = "Black", bd = 10, anchor='w') 
+                       
+lblInfo.grid(row = 0, column = 0) 
+  
+lblInfo = Label(Tops, font=('arial', 20, 'bold'), 
+             text = datetime.now().strftime("%d-%m-%Y, %I:%M:%S %p"), fg = "Steel Blue", 
+                           bd = 10, anchor = 'w') 
+                          
+lblInfo.grid(row = 1, column = 0) 
+
+Msg = StringVar() 
+name = StringVar() 
+key = StringVar() 
+mode = StringVar() 
+Result = StringVar() 
+  
+# exit function 
+def Exit(): 
+    root.destroy() 
+  
+# Function to reset the window 
+def Reset():  
+    Msg.set("") 
+    name.set("")
+    key.set("") 
+    mode.set("") 
+    Result.set("") 
+
+# labels 
+lblname = Label(f1, font = ('arial', 16, 'bold'), 
+                text = "Name:", bd = 16, anchor = "w") 
+
+lblname.grid(row = 1, column = 0) 
+
+txtname = Entry(f1, font = ('arial', 16, 'bold'), 
+         textvariable = name, bd = 10, insertwidth = 4, 
+                bg = "powder blue", justify = 'right') 
+                  
+txtname.grid(row = 1, column = 1) 
+
+lblMsg = Label(f1, font = ('arial', 16, 'bold'), 
+         text = "MESSAGE", bd = 16, anchor = "w") 
+           
+lblMsg.grid(row = 2, column = 0) 
+  
+txtMsg = Entry(f1, font = ('arial', 16, 'bold'), 
+                textvariable = Msg, bd = 10, insertwidth = 4, 
+                bg = "powder blue", justify = 'right') 
+                  
+txtMsg.grid(row = 2, column = 1) 
+  
+lblkey = Label(f1, font = ('arial', 16, 'bold'), 
+                   text = "KEY", bd = 16, anchor = "w") 
+              
+lblkey.grid(row = 3, column = 0) 
+  
+txtkey = Entry(f1, font = ('arial', 16, 'bold'), 
+                   textvariable = key, bd = 10, insertwidth = 4, 
+                   bg = "powder blue", justify = 'right') 
+                  
+txtkey.grid(row = 3, column = 1) 
+  
+lblmode = Label(f1, font = ('arial', 16, 'bold'), 
+                    text = "MODE(Encrypt or Decrypt)", 
+                    bd = 16, anchor = "w") 
+                                  
+lblmode.grid(row = 4, column = 0) 
+  
+txtmode = Entry(f1, font = ('arial', 16, 'bold'), 
+                    textvariable = mode, bd = 10, insertwidth = 4, 
+                    bg = "powder blue", justify = 'right') 
+                    
+txtmode.grid(row = 4, column = 1) 
+  
+lblService = Label(f1, font = ('arial', 16, 'bold'), 
+                       text = "The Result", bd = 16, anchor = "w") 
+
+lblService.grid(row = 2, column = 2) 
+  
+txtService = Entry(f1, font = ('arial', 16, 'bold'),  
+                       textvariable = Result, bd = 10, insertwidth = 4, 
+                       bg = "powder blue", justify = 'right') 
+                         
+txtService.grid(row = 2, column = 3) 
+  
+# Function to encode 
+def encode(key, clear): 
+    enc = [] 
+      
+    for i in range(len(clear)): 
+        key_c = key[i % len(key)] 
+        enc_c = chr((ord(clear[i]) +
+                     ord(key_c)) % 256) 
+                       
+        enc.append(enc_c) 
+          
+    return encoder_and_decoder.urlsafe_b64encode("".join(enc).encode()).decode() 
+  
+# Function to decode 
+def decode(key, enc): 
+    dec = [] 
+      
+    enc = encoder_and_decoder.urlsafe_b64decode(enc).decode() 
+    for i in range(len(enc)): 
+        key_c = key[i % len(key)] 
+        dec_c = chr((256 + ord(enc[i]) -
+                           ord(key_c)) % 256) 
+                             
+        dec.append(dec_c) 
+    return "".join(dec) 
+
+# Function to log
+def log(msg):
+    file_log = open(logging_file_path,"a+") 
+    msg = "\n\n\n" + (datetime.now().strftime("%d-%m-%Y, %I:%M:%S %p") + "\n\n" + msg)
+    file_log.write(msg) 
+    file_log.close()
+
+def Output(): 
+    clear = Msg.get()
+    n = name.get()
+    k = key.get() 
+    m = mode.get().lower() 
+
+    if m == 'encrypt' or m == 'e':
+        # Log's the text if mode is equal to encryption.
+        log(f"Name: {n}\nMessage: {encode(k + n, clear)}\nKey: {k}")
+        Result.set(encode(k + n, clear)) 
+    elif m == 'decrypt' or m == 'd': 
+        Result.set(decode(k + n, clear))
+  
+# Show message button 
+btnTotal = Button(f1, padx = 16, pady = 8, bd = 16, fg = "black", 
+                        font = ('arial', 16, 'bold'), width = 10, 
+                        text = "Show Message", bg = "powder blue", 
+                        command = Output).grid(row = 7, column = 1)
+  
+# Reset button 
+btnReset = Button(f1, padx = 16, pady = 8, bd = 16, 
+                    fg = "black", font = ('arial', 16, 'bold'), 
+                    width = 10, text = "Reset", bg = "green", 
+                    command = Reset).grid(row = 7, column = 2) 
+  
+# Exit button 
+btnExit = Button(f1, padx = 16, pady = 8, bd = 16,  
+                    fg = "black", font = ('arial', 16, 'bold'), 
+                    width = 10, text = "Exit", bg = "red", 
+                    command = Exit).grid(row = 7, column = 3) 
+  
+# keeps window alive 
+root.mainloop() 
