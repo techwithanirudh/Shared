caa65bd39ed6cbafca5402bd4124da37663fb8e7
techwithanirudh <70373803+techwithanirudh@users.noreply.github.com>
2020-12-21 09:09:19 +0530

    Create shuffle word.py

---
+++ shuffle word.py
@@ -0,0 +1,26 @@
+from spellchecker import SpellChecker
+from random import shuffle
+
+spell = SpellChecker()
+wordInp = input('Enter a word: ')
+times = 0
+
+def shuffle_word(word):
+    word = list(word)
+    shuffle(word)
+    return ''.join(word)
+
+word = shuffle_word(wordInp)
+while True:
+    word = shuffle_word(word)
+    corrected = spell.correction(word)
+    if corrected != word:
+        times += 1 
+        print(corrected)
+        if len(corrected) == len(word):
+            correctedL = list(corrected)
+            if all(letter in correctedL for letter in word):
+                if corrected == wordInp:
+                    print('Times: ', times)
+                    print('Word Is:', corrected)
+                    break
