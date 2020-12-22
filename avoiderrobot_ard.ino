fc57148d677ba7dd09bd8f8ac3df720b2b20acbd
techwithanirudh <70373803+techwithanirudh@users.noreply.github.com>
2020-12-22 17:56:17 +0530

    Update avoiderrobot_ard.ino

--- avoiderrobot_ard.ino
+++ avoiderrobot_ard.ino
@@ -26,12 +26,41 @@ long readUltrasonicDistance(int triggerPin, int echoPin)
 
 void loop()
 {
+   digitalWrite(8, LOW);
+   digitalWrite(9, HIGH);
+   digitalWrite(10, LOW);
+   digitalWrite(11, HIGH);
    int i=0;
    for (i=0; i<180; i++)
    {
      MyServoMotor1.write(i);
      int val = 0.01723 * readUltrasonicDistance(4, 3);
      Serial.println(val);
+     if val < 20
+     {
+        Serial.println('Going Back and Turning');
+        digitalWrite(8, HIGH);
+        digitalWrite(9, LOW);
+        digitalWrite(10, HIGH);
+        digitalWrite(11, LOW);
+        delay(1000);
+        int rand = random(1)
+        Serial.println(rand);
+        if rand
+        {
+            digitalWrite(8, HIGH);
+            digitalWrite(9, LOW);
+            digitalWrite(10, LOW);
+            digitalWrite(11, HIGH);
+        }
+        else
+        {
+            digitalWrite(8, LOW);
+            digitalWrite(9, HIGH);
+            digitalWrite(10, HIGH);
+            digitalWrite(11, LOW);
+        }
+     }
      delay(10);
    }
    for (i=180; i>0; i--)
@@ -39,6 +68,31 @@ void loop()
      MyServoMotor1.write(i);
      int val = 0.01723 * readUltrasonicDistance(4, 3);
      Serial.println(val);
+     if val < 20
+     {
+        Serial.println('Going Back and Turning');
+        digitalWrite(8, HIGH);
+        digitalWrite(9, LOW);
+        digitalWrite(10, HIGH);
+        digitalWrite(11, LOW);
+        delay(1000);
+        int rand = random(1)
+        Serial.println(rand);
+        if rand
+        {
+            digitalWrite(8, HIGH);
+            digitalWrite(9, LOW);
+            digitalWrite(10, LOW);
+            digitalWrite(11, HIGH);
+        }
+        else
+        {
+            digitalWrite(8, LOW);
+            digitalWrite(9, HIGH);
+            digitalWrite(10, HIGH);
+            digitalWrite(11, LOW);
+        }
+     }
      delay(10);
    }  
 }
