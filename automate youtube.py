289a068c8b14b81ada2fb7dcbba1368558934f04
Anirudh <70373803+techwithanirudh@users.noreply.github.com>
2021-01-31 13:40:16 +0530

    Update automate youtube.py

--- automate youtube.py
+++ automate youtube.py
@@ -4,6 +4,9 @@ import os
 from myModules import browser
 from selenium.webdriver.firefox.options import Options
 import time
+from email.message import EmailMessage
+import smtplib
+import imghdr
 
 # from selenium import webdriver
 
@@ -12,8 +15,25 @@ import time
 #     browser.maximize_window()
 #     return browser
 
+_from = ''
+pwd = ''
+to = ''
+
 os.chdir(os.path.dirname(__file__))
 open('titles.txt', 'a').close()
+
+def send_email(receiver, subject, message, img_data=None):
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login(_from, pwd)
+    email = EmailMessage()
+    email['From'] = _from
+    email['To'] = receiver
+    email['Subject'] = subject
+    email.set_content(message)
+    if img_data != None:
+        email.add_attachment(img_data, maintype='image', subtype=imghdr.what(None, img_data))
+    server.send_message(email)
 
 def post_to_twitter():
     pass
@@ -49,7 +69,12 @@ while True:
         browser.get('https://www.youtube.com/')
         thumbnail_url = 'https:///img.youtube.com/vi/' + id + '/maxresdefault.jpg'
         image_path = download_thumbnail(thumbnail_url)
+        img_file = 'thumbnail_youtube.png'
+        with open(img_file, 'rb') as f:
+            img_data = f.read()
+        send_email(to, 'Youtube', 'This is the thumbnail and title of your new video', img_data)
         F = open('titles.txt', 'w')
         F.write(video_title)
         F.close()
+
     time.sleep(60 * mins)
