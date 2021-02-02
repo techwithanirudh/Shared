74868f755aa5151494bbf70765a3ebae6451c284
Anirudh <70373803+techwithanirudh@users.noreply.github.com>
2021-02-02 14:33:54 +0530

    Create chatbot.py

---
+++ chatbot.py
@@ -0,0 +1,102 @@
+import nltk
+from nltk.stem import WordNetLemmatizer
+lemmatizer = WordNetLemmatizer()
+import spacy 
+import pickle
+import numpy as np
+import os
+os.chdir(os.path.dirname(__file__))
+
+from keras.models import load_model
+model = load_model('chatbot_model.h5')
+
+import json
+import random
+
+intents = json.loads(open('intents.json').read())
+words = pickle.load(open('words.pkl','rb'))
+classes = pickle.load(open('classes.pkl','rb'))
+
+def clean_up_sentence(sentence):
+    sentence_words = nltk.word_tokenize(sentence)
+    sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
+    return sentence_words
+
+# Return bag of words array: 0 or 1 for each word in the bag that exists in the sentence
+
+def bow(sentence, words, show_details=True):
+    # Tokenize the pattern
+    sentence_words = clean_up_sentence(sentence)
+    # Bag of words - matrix of N words, vocabulary matrix
+    bag = [0]*len(words)
+    for s in sentence_words:
+        for i,w in enumerate(words):
+            if w == s:
+                # Assign 1 if current word is in the vocabulary position
+                bag[i] = 1
+                if show_details:
+                    print('found in bag: %s' % w)
+    return(np.array(bag))
+
+def predict_class(sentence, model):
+    # Filter out predictions below a threshold
+    p = bow(sentence, words,show_details=False)
+    res = model.predict(np.array([p]))[0]
+    ERROR_THRESHOLD = 0.93
+    results = [[i,r] for i,r in enumerate(res) if r > ERROR_THRESHOLD]
+    # Sort by strength of probability
+    results.sort(key=lambda x: x[1], reverse=True)
+    return_list = []
+    for r in results:
+        return_list.append({'intent': classes[r[0]], 'probability': str(r[1])})
+
+    print(return_list)
+    return return_list
+
+def getResponse(ints, intents_json):
+    nlp = spacy.load('en_core_web_sm') 
+    doc = nlp(msgs) 
+    if ints:
+        tag = ints[0]['intent']
+        list_of_intents = intents_json['intents']
+        for i in list_of_intents:
+            if(i['tag']== tag):
+                result = random.choice(i['responses'])
+                cTag = i['tag']
+                break
+
+        textL = []
+        labelL = []
+
+        for ent in doc.ents: 
+            label = ent.label_
+            text = ent.text
+            textL.append(text)
+            labelL.append(label)
+            print(label + ':', text)
+
+        return entity(textL, labelL, cTag, result)
+    else:
+        return random.choice(['Sorry, can\'t understand you', 'Please give me more info', 'Not sure I understand'])
+
+def chatbot_response(msg):
+    global msgs
+    msgs = msg
+    ints = predict_class(msg, model)
+    res = getResponse(ints, intents)
+    return res
+
+def weather(lblL, ent, result):
+    return result.replace('(.*)', ent) + 'is very good'
+
+def entity(entL, lblL, tag, result):
+    entTags = ['weather']
+    for ent in entL:
+        if tag in entTags:
+            return eval(f'{tag}(lblL, ent, result)')
+        else:
+            return result
+
+while True:
+    msg = input('You: ')
+    print(f'Bot: {chatbot_response(msg)}')
