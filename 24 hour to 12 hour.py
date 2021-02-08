a347b425eb7efac4317047da58079840aa6a219b
Anirudh <70373803+techwithanirudh@users.noreply.github.com>
2021-02-08 10:01:05 +0530

    Create 24 hour to 12 hour.py

---
+++ 24 hour to 12 hour.py
@@ -0,0 +1,23 @@
+time = input('Time: ')
+# time = '18:15'
+'06:15'
+
+timeH = time.split(':')[0]
+timeM = time.split(':')[1]
+
+if int(timeH) > 12:
+    if int(timeH) < 20:
+        _timeH = int(timeH[1]) - 2
+        _timeH = str(_timeH)
+        _FtimeH = '0' + _timeH
+    else:
+        _timeH = int(timeH) - 2
+        _FtimeH = str(_timeH)[1]
+        if int(_timeH) > 19:
+            _FtimeH = '1' + _FtimeH
+        else:
+            _FtimeH = '0' + _FtimeH
+else:
+    _FtimeH = timeH
+
+print(_FtimeH + ':' + timeM)
