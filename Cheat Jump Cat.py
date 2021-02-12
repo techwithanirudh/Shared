40ca6d3f3d0a0da79f26fb7cbe13ca3f6051af12
Anirudh <70373803+techwithanirudh@users.noreply.github.com>
2021-02-12 13:38:05 +0530

    Update Cheat Jump Cat.py

--- Cheat Jump Cat.py
+++ Cheat Jump Cat.py
@@ -4,9 +4,10 @@ pyautogui.keyDown('space')
 time.sleep(0.5) 
 pyautogui.keyUp('space')
 time.sleep(3.3)
+points = 10
 
-# Get Up To 64 Score If You Want Unlimited Scores replace for i in range(30): with while True:
-for i in range(30): 
+# Get Up To 64 Score If You Want Unlimited Scores replace for i in range(30) with while True:
+for i in range(int(points / 2 - 2)): 
     pyautogui.keyDown('space')
     time.sleep(0.5) 
     pyautogui.keyUp('space')
