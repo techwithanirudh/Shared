d4355a120d5c8c933a648175515f5222d40d478b
Anirudh <70373803+techwithanirudh@users.noreply.github.com>
2021-03-14 19:08:52 +0530

    Add files via upload

---
+++ RecordFace.py
@@ -0,0 +1,45 @@
+import cv2
+import dlib
+import os
+import numpy as np
+
+path = os.path.dirname(__file__)
+os.chdir(path)
+
+cap = cv2.VideoCapture(0)
+detector = dlib.get_frontal_face_detector()
+
+# https://stackoverflow.com/a/49983255/14777961
+def imcrop(img, bbox): 
+    x1,y1,x2,y2 = bbox
+    if x1 < 0 or y1 < 0 or x2 > img.shape[1] or y2 > img.shape[0]:
+        img, x1, x2, y1, y2 = pad_img_to_fit_bbox(img, x1, x2, y1, y2)
+    return img[y1:y2, x1:x2, :]
+
+def pad_img_to_fit_bbox(img, x1, x2, y1, y2):
+    img = np.pad(img, ((np.abs(np.minimum(0, y1)), np.maximum(y2 - img.shape[0], 0)),
+               (np.abs(np.minimum(0, x1)), np.maximum(x2 - img.shape[1], 0)), (0,0)), mode='constant')
+    y1 += np.abs(np.minimum(0, y1))
+    y2 += np.abs(np.minimum(0, y1))
+    x1 += np.abs(np.minimum(0, x1))
+    x2 += np.abs(np.minimum(0, x1))
+    return img, x1, x2, y1, y2
+
+while True:
+	_, frame = cap.read()
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	faces = detector(gray)
+	for face in faces:
+		x1 = face.left()
+		y1 = face.top()
+		x2 = face.right()
+		y2 = face.bottom()
+        # cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 3)
+		bbox = (x1, y1, x2, y2)
+		frame = imcrop(frame, bbox)
+
+	cv2.imshow('Frame', frame)
+
+	key = cv2.waitKey(20)
+	if key == 27:
+	    break
