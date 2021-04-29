7ce68e2b63e2866b62f4c2c5cedbc9804b9d50d5
Anirudh <70373803+techwithanirudh@users.noreply.github.com>
2021-04-29 13:08:41 +0530

    Update word(Un)Scrambler.py

--- word(Un)Scrambler.py
+++ word(Un)Scrambler.py
@@ -24,22 +24,27 @@ if sGen:
 s = time.time()
 
 while True:
-    word = shuffle_word(word)
-    corrected = spell.correction(word)
-    if corrected != word:
-        times += 1 
-        print('Trying:', corrected)
-        if len(corrected) == len(word):
-            correctedL = list(corrected)
-            if all(letter in correctedL for letter in word):
-                c = time.time()
-                print('Seconds:', c - s)
-                print('Times:', times)
-                print('Unscrambled Word Is:', corrected)
-                if sGen: print('Scrambled Word Is:', wordShu)
-                correct = input('Is this correct (Y/N)? ').lower()
-                if correct == 'y': correct = True
-                if correct == 'n': correct = False
-                if correct: break
-                os.system('cls')
-                print('Enter a word:', wordInp)
+    try:
+        word = shuffle_word(word)
+        corrected = spell.correction(word)
+        if corrected != word:
+            times += 1 
+            print('Trying:', corrected)
+            if len(corrected) == len(word):
+                correctedL = list(corrected)
+                if all(letter in correctedL for letter in word):
+                    c = time.time()
+                    print('Seconds:', c - s)
+                    print('Times:', times)
+                    print('Unscrambled Word Is:', corrected)
+                    if sGen: print('Scrambled Word Is:', wordShu)
+                    correct = input('Is this correct (Y/N)? ').lower()
+                    if correct == 'y': correct = True
+                    if correct == 'n': correct = False
+                    if correct: break
+                    os.system('cls')
+                    print('Enter a word:', wordInp)
+    except KeyboardInterrupt:
+        os.system('cls')
+        print('Exiting Program...')
+        break
